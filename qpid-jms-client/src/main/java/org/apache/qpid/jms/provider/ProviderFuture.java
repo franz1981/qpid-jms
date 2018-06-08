ce3fa2c84df5509d8aee9e7385d00289de6c6d32
Francesco Nigro <nigro.fra@gmail.com>
2018-06-08 18:40:33 +0200

    Added progressive wait strategy to make ProviderFuture::onFailure/onSuccess more cheaper and sync more reactive

--- qpid-jms-client/src/main/java/org/apache/qpid/jms/provider/ProviderFuture.java
+++ qpid-jms-client/src/main/java/org/apache/qpid/jms/provider/ProviderFuture.java
@@ -16,22 +16,32 @@
  */
 package org.apache.qpid.jms.provider;
 
+import org.apache.qpid.jms.util.IOExceptionSupport;
+
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.apache.qpid.jms.util.IOExceptionSupport;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * Asynchronous Provider Future class.
  */
 public class ProviderFuture implements AsyncResult {
 
-    private final AtomicBoolean completer = new AtomicBoolean();
-    private final CountDownLatch latch = new CountDownLatch(1);
+    //Using a progressive wait strategy help to avoid await to happens before countDown
+    //and avoid expensive thread signaling
+    private static final int SPIN_COUNT = 10;
+    private static final int YIELD_COUNT = 100;
+    private static final int TINY_PARK_COUNT = 1000;
+    private static final int TINY_PARK_NANOS = 1;
+    private static final int SMALL_PARK_COUNT = 101_000;
+    private static final int SMALL_PARK_NANOS = 10_000;
+    private static final AtomicIntegerFieldUpdater<ProviderFuture> COMPLETER_FIELD_UPDATER = AtomicIntegerFieldUpdater.newUpdater(ProviderFuture.class,"completer");
+    private volatile int completer = 0;
+    private final CountDownLatch finished = new CountDownLatch(1);
     private final ProviderSynchronization synchronization;
-    private volatile Throwable error;
+    private Throwable error;
 
     public ProviderFuture() {
         this(null);
@@ -43,27 +53,29 @@ public class ProviderFuture implements AsyncResult {
 
     @Override
     public boolean isComplete() {
-        return latch.getCount() == 0;
+        return completer > 0;
     }
 
     @Override
     public void onFailure(Throwable result) {
-        if (completer.compareAndSet(false, true)) {
+        if (COMPLETER_FIELD_UPDATER.compareAndSet(this, 0, -1)) {
             error = result;
             if (synchronization != null) {
                 synchronization.onPendingFailure(error);
             }
-            latch.countDown();
+            finished.countDown();
+            COMPLETER_FIELD_UPDATER.lazySet(this, 2);
         }
     }
 
     @Override
     public void onSuccess() {
-        if (completer.compareAndSet(false, true)) {
+        if (COMPLETER_FIELD_UPDATER.compareAndSet(this, 0, -1)) {
             if (synchronization != null) {
                 synchronization.onPendingSuccess();
             }
-            latch.countDown();
+            finished.countDown();
+            COMPLETER_FIELD_UPDATER.lazySet(this, 1);
         }
     }
 
@@ -81,16 +93,51 @@ public class ProviderFuture implements AsyncResult {
      * @throws IOException if an error occurs while waiting for the response.
      */
     public boolean sync(long amount, TimeUnit unit) throws IOException {
-        boolean result = false;
         try {
-            result = latch.await(amount, unit);
+            if (isComplete()) {
+                failOnError();
+                return true;
+            }
+            final Thread currentThread = Thread.currentThread();
+            final long timeout = unit.toNanos(amount);
+            long maxParkNanos = timeout / 8;
+            maxParkNanos = maxParkNanos > 0 ? maxParkNanos : timeout;
+            final long tinyParkNanos = Math.min(maxParkNanos, TINY_PARK_NANOS);
+            final long smallParkNanos = Math.min(maxParkNanos, SMALL_PARK_NANOS);
+            final long now = System.nanoTime();
+            int idleCount = 0;
+            while (true) {
+                if (currentThread.isInterrupted()) {
+                    throw new InterruptedException();
+                }
+                final long elapsed = System.nanoTime() - now;
+                final long diff = elapsed - timeout;
+                if (diff > 0) {
+                    return false;
+                }
+                if (isComplete()) {
+                    failOnError();
+                    return true;
+                }
+                if (idleCount < SPIN_COUNT) {
+                    idleCount++;
+                } else if (idleCount < YIELD_COUNT) {
+                    Thread.yield();
+                    idleCount++;
+                } else if (idleCount < TINY_PARK_COUNT) {
+                    LockSupport.parkNanos(tinyParkNanos);
+                    idleCount++;
+                } else if (idleCount < SMALL_PARK_COUNT) {
+                    LockSupport.parkNanos(smallParkNanos);
+                    idleCount++;
+                } else {
+                    finished.await(-diff, TimeUnit.NANOSECONDS);
+                }
+            }
         } catch (InterruptedException e) {
             Thread.interrupted();
             throw IOExceptionSupport.create(e);
         }
-        failOnError();
-
-        return result;
     }
 
     /**
@@ -100,12 +147,39 @@ public class ProviderFuture implements AsyncResult {
      */
     public void sync() throws IOException {
         try {
-            latch.await();
+            if (isComplete()) {
+                failOnError();
+                return;
+            }
+            final Thread currentThread = Thread.currentThread();
+            int idleCount = 0;
+            while (true) {
+                if (currentThread.isInterrupted()) {
+                    throw new InterruptedException();
+                }
+                if (isComplete()) {
+                    failOnError();
+                    return;
+                }
+                if (idleCount < SPIN_COUNT) {
+                    idleCount++;
+                } else if (idleCount < YIELD_COUNT) {
+                    Thread.yield();
+                    idleCount++;
+                } else if (idleCount < TINY_PARK_COUNT) {
+                    LockSupport.parkNanos(TINY_PARK_NANOS);
+                    idleCount++;
+                } else if (idleCount < SMALL_PARK_COUNT) {
+                    LockSupport.parkNanos(SMALL_PARK_NANOS);
+                    idleCount++;
+                } else {
+                    finished.await();
+                }
+            }
         } catch (InterruptedException e) {
             Thread.interrupted();
             throw IOExceptionSupport.create(e);
         }
-        failOnError();
     }
 
     private void failOnError() throws IOException {
